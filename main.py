4a001ee2ba579de9f606d1d895d71bb433813c06
cmmira <>
2024-11-28 00:24:50 -0500

    Adding Navigation Links

--- main.py
+++ main.py
@@ -11,5 +11,17 @@ Bootstrap5(app)
 def info():
     return render_template("index.html", me=me)
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template("portfolio.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
